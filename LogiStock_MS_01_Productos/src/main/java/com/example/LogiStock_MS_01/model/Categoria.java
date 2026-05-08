31c3ffb2faab800f61234298b8b1b6b544f60ec0
VA- Alumno <VA-Alumno@duoc.acad>
2026-05-08 19:51:35 -0400

    MS_03: Estructura corregida, patrón CSR y compilación exitosa

---
+++ LogiStock_MS_01_Productos/src/main/java/com/example/LogiStock_MS_01/model/Categoria.java
@@ -0,0 +1,26 @@
+package com.example.LogiStock_MS_01.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+
+@Entity
+@Table(name = "categoria")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+
+}
