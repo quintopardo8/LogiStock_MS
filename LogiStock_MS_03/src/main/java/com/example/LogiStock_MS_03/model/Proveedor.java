f4864dcc8a768ffd325a4be2443cca4ff09a30e3
mpinol02 <mar.pinol@duocuc.cl>
2026-05-08 20:51:54 -0400

    ffygy

--- LogiStock_MS_03/src/main/java/com/example/LogiStock_MS_03/model/Proveedor.java
+++ LogiStock_MS_03/src/main/java/com/example/LogiStock_MS_03/model/Proveedor.java
@@ -7,7 +7,7 @@ import lombok.Data;
 @Table(name = "MS03_PROVEEDORES") 
 @Data 
 public class Proveedor {
-
+//lokokoo
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
