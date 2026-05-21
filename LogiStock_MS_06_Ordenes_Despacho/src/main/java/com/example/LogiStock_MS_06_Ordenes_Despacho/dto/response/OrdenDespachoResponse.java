7721957c9116c89627046be87b8930a27d43cec3
mpinol02 <mar.pinol@duocuc.cl>
2026-05-20 21:36:20 -0400

    Update OrdenDespachoResponse.java

--- LogiStock_MS_06_Ordenes_Despacho/src/main/java/com/example/LogiStock_MS_06_Ordenes_Despacho/dto/response/OrdenDespachoResponse.java
+++ LogiStock_MS_06_Ordenes_Despacho/src/main/java/com/example/LogiStock_MS_06_Ordenes_Despacho/dto/response/OrdenDespachoResponse.java
@@ -14,3 +14,26 @@ public class OrdenDespachoResponse {
     private LocalDateTime fechaCreacion;
     private LocalDateTime fechaEnvio;
 }
+
+
+public class OrdenDespachoResponse {
+    private Long id;
+    private String numeroSeguimiento;
+    private Cliente cliente;
+    private String direccionDespacho;
+    private EstadoDespacho estado;
+    private LocalDateTime fechaCreacion;
+    private LocalDateTime fechaEnvio;
+}
+
+
+@Data
+public class OrdenDespachoResponse {
+    private Long id;
+    private String numeroSeguimiento;
+    private Cliente cliente; // <-- Vincula la clase que arreglamos en el Paso 1
+    private String direccionDespacho;
+    private EstadoDespacho estado;
+    private LocalDateTime fechaCreacion;
+    private LocalDateTime fechaEnvio;
+}
