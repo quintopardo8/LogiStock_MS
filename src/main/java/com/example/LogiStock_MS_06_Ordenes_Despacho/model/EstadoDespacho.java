f0247c5d378c55f10595dd60ae68c7d3ac443fa3
mpinol02 <mar.pinol@duocuc.cl>
2026-05-27 17:13:50 -0400

    Limpiar archivos sueltos de la raíz y mantener carpeta del MS06

--- src/main/java/com/example/LogiStock_MS_06_Ordenes_Despacho/model/EstadoDespacho.java
+++
@@ -1,9 +0,0 @@
-package com.example.LogiStock_MS_06_Ordenes_Despacho.model;
-
-public enum EstadoDespacho {
-    PENDIENTE,  
-    PREPARANDO, 
-    DESPACHADO,  
-    ENTREGADO,  
-    CANCELADO   
-}
